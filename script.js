05171251a80fe43a0feb69e3978dd569b2742a42
Hassan Alayan <66628421+7sn-al@users.noreply.github.com>
2025-08-20 19:45:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,19 @@ const tvGenres = {
 
 
 window.addEventListener('DOMContentLoaded', () => {
+
+const defaultPage= function(){
+  showMovies(MOVIELINK, '#row1 .grid-wrapper'); 
+showMovies(GENRELINK('action'), '#row2 .grid-wrapper');
+showMovies(GENRELINK('comedy') ,'#row3 .grid-wrapper');
+showMovies(GENRELINK('horror'), '#row4 .grid-wrapper');
+
+showMovies(TVLINK, '#row5 .grid-wrapper'); 
+showMovies(TVGENRELINK('actionAdventure'), '#row6 .grid-wrapper');
+showMovies(TVGENRELINK('comedy') ,'#row7 .grid-wrapper');
+showMovies(TVGENRELINK('animation'), '#row8 .grid-wrapper');
+}
+
 const header = document.querySelector('.header');
   window.addEventListener('scroll', function(){
     if (window.scrollY > 50) { 
@@ -64,6 +77,7 @@ const header = document.querySelector('.header');
   const MOVIELINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=61a8d5b0281dfd932f4e3a6339f7e6be&page=1';
   const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
   const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?api_key=61a8d5b0281dfd932f4e3a6339f7e6be&query=';
+  const TVSEARCHAPI = 'https://api.themoviedb.org/3/search/tv?api_key=61a8d5b0281dfd932f4e3a6339f7e6be&query=';
   const TVLINK = 'https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=61a8d5b0281dfd932f4e3a6339f7e6be&page=1';
  
   const GENRELINK = function(genre){
@@ -120,16 +134,36 @@ rows.forEach(row => {
   });
 });
 
-showMovies(MOVIELINK, '#row1 .grid-wrapper'); 
-showMovies(GENRELINK('action'), '#row2 .grid-wrapper');
-showMovies(GENRELINK('comedy') ,'#row3 .grid-wrapper');
-showMovies(GENRELINK('horror'), '#row4 .grid-wrapper');
+ defaultPage();
 
-showMovies(TVLINK, '#row5 .grid-wrapper'); 
-showMovies(TVGENRELINK('actionAdventure'), '#row6 .grid-wrapper');
-showMovies(TVGENRELINK('comedy') ,'#row7 .grid-wrapper');
-showMovies(TVGENRELINK('animation'), '#row8 .grid-wrapper');
+const input=document.querySelector('.site-search');
 
+input.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const query = input.value.trim();
+        if(query){
+          showMovies(SEARCHAPI + query, '#row1 .grid-wrapper');
+
+          const rows = document.querySelectorAll('.row-container');
+          rows.forEach(row => row.style.display = "none");
+
+          const row1 = document.querySelector('#row1');
+          row1.style.display = "";
+
+          const row5 = document.querySelector('#row5');
+          row5.style.display = "";
+          showMovies(TVSEARCHAPI + query , '#row5 .grid-wrapper');
+          input.value='';
+
+         }
+         else if(!query){
+          defaultPage();
+          rows.forEach(row=> row.style.display='')
+         }
+    }
+  }
+);
 
 });
 
